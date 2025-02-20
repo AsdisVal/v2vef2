edec8649a2ba4c4eb0815690d2d59900384f5422
Ásdís Valtýsdóttir <asdisval@gmail.com>
2025-02-20 16:59:38 +0000

    skemur

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,3 +3,15 @@ CREATE TABLE IF NOT EXISTS public.categories (
     name varchar(64) not null unique,
     created timestamp with time zone not null default current_timestamp
 );
+CREATE TABLE IF NOT EXISTS public.questions (
+    id serial primary key,
+    category_id integer not null references public.categories(id),
+    question text not null,
+    created timestamp with time zone not null default current_timestamp
+);
+CREATE TABLE IF NOT EXISTS public.answers (
+    id serial primary key,
+    question_id integer not null references public.questions(id),
+    answer text not null,
+    created timestamp with time zone not null default current_timestamp
+);
