a9a90ec6cc4c137a47d8bf7e9c6ed35b17113b98
Ásdís Valtýsdóttir <asdisval@gmail.com>
2025-02-21 12:13:01 +0000

    spurningar

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,22 +1,19 @@
 CREATE TABLE IF NOT EXISTS public.categories (
     id serial primary key,
     name varchar(64) not null unique,
-    created timestamp with time default current_timestamp not null
 );
 CREATE TABLE IF NOT EXISTS public.questions (
     id serial primary key,
     category_id integer not null references public.categories(id) on delete cascade,
-    question_text text not null check (
-        char_length(question_text) between 10 and 500
-    ),
-    created timestamp with time zone default current_timestamp not null
+    question text not null check (
+        char_length(question) between 10 and 500
+    )
 );
 CREATE TABLE IF NOT EXISTS public.answers (
     id serial primary key,
     question_id integer not null references public.questions(id) on delete cascade,
-    answer_text TEXT NOT NULL CHECK (
-        char_length(answer_text) BETWEEN 1 AND 255
+    answer TEXT NOT NULL CHECK (
+        char_length(answer) BETWEEN 1 AND 255
     ),
-    is_correct boolean default false not null,
-    created timestamp with time zone default current_timestamp not null
+    is_correct boolean default false not null
 );
