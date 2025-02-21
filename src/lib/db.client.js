ef430c46014ae54cadefc141facb988f63d2a8f9
Ásdís Valtýsdóttir <asdisval@gmail.com>
2025-02-21 13:00:06 +0000

    db client

--- src/lib/db.client.js
+++ src/lib/db.client.js
@@ -20,13 +20,11 @@ export class Database {
   pool = null;
 
   open() {
-    if (!this.pool) {
-      this.pool = new pg.Pool({ connectionString: this.connectionString });
-      this.pool.on('error', (err) => {
-        this.logger.error('Database pool error:', err);
-        this.close();
-      });
-    }
+    this.pool = new pg.Pool({ connectionString: this.connectionString });
+    this.pool.on('error', (err) => {
+      this.logger.error('Database pool error:', err);
+      this.close();
+    });
   }
 
   /**
@@ -90,72 +88,6 @@ export class Database {
     } finally {
       client.release();
     }
-  }
-
-  async getCategories() {
-    return (
-      (await this.query('SELECT id, name FROM categories ORDER BY name ASC'))
-        ?.rows ?? []
-    );
-  }
-
-  async insertCategory(name) {
-    const existing = await this.query(
-      'SELECT id FROM categories WHERE name = $1',
-      [name]
-    );
-    if (existing?.rows.length) return null;
-
-    return (
-      (
-        await this.query(
-          'INSERT INTO categories (name) VALUES ($1) RETURNING id, name',
-          [name]
-        )
-      )?.rows[0] ?? null
-    );
-  }
-
-  // þetta myndi meika sens held ég ef ég gæti náð að tengja gögnin við þetta lol
-  async getQuestionsByCategory(categoryId) {
-    return (
-      (
-        await this.query(
-          `SELECT q.id, q.question_text, 
-        json_agg(json_build_object('id', a.id, 'text', a.answer_text, 'is_correct', a.is_correct)) AS answers
-      FROM questions q
-      LEFT JOIN answers a ON q.id = a.question_id
-      WHERE q.category_id = $1
-      GROUP BY q.id
-      ORDER BY q.id DESC;`,
-          [categoryId]
-        )
-      )?.rows ?? []
-    );
-  }
-
-  //new question
-  async insertQuestion(text, categoryId) {
-    return (
-      (
-        await this.query(
-          'INSERT INTO questions (question_text, category_id) VALUES ($1, $2) RETURNING id',
-          [text, categoryId]
-        )
-      )?.rows[0] ?? null
-    );
-  }
-
-  //an answer
-  async insertAnswer(questionId, text, isCorrect) {
-    return (
-      (
-        await this.query(
-          'INSERT INTO answers (question_id, answer_text, is_correct) VALUES ($1, $2, $3) RETURNING id',
-          [questionId, text, isCorrect]
-        )
-      )?.rows[0] ?? null
-    );
   }
 }
 
