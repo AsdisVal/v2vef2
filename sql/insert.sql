75e0fcc0b372cf18196a5aa871e4727b2fee09e5
Ásdís Valtýsdóttir <asdisval@gmail.com>
2025-02-23 01:27:30 +0000

    allt komid nu a ensku

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,127 +1,127 @@
-insert into flokkar (nafn)
+insert into categories (name)
 values ('html');
-insert into flokkar (nafn)
+insert into categories (name)
 values ('css');
-insert into flokkar (nafn)
+insert into categories (name)
 values ('javascript');
 --html
-INSERT INTO spurningar (flokkur_id, spurning)
+INSERT INTO questions (category_id, text)
 VALUES (
         1,
         'Ef við værum að smíða vefsíðu og myndum vilja geta farið frá index.html yfir á about.html, hvað væri best að nota?'
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         1,
         '<a href=\"about.html\">About</a>',
         true
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         1,
         '<form method=\"get\" action=\"about.html\"><button>About</button></form>',
         false
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         1,
         '<button to=\"about.html\">About</button>',
         false
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         1,
         'Allar jafn góðar / All equally good',
         false
     );
-INSERT INTO spurningar (flokkur_id, spurning)
+INSERT INTO questions (category_id, text)
 VALUES (
         1,
         'Í <head> á vefjum setjum við <meta charset=\"utf-8\"> (eða það stafasett sem nota á), afhverju er það gert?'
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         2,
         'Þannig að stafir birtist rétt.',
         true
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         2,
         'Skilgreining sem visual studio verður að hafa þannig að prettier virki rétt.',
         false
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         2,
         'Skilgreining sem aXe krefur okkur um til að vefur verði aðgengilegur.',
         false
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         2,
         'Ekkert af þessu.',
         false
     );
-INSERT INTO spurningar (flokkur_id, spurning)
+INSERT INTO questions (category_id, text)
 VALUES (
         1,
         'Það sem við getum gert til að forrita aðgengilega vefi er'
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         3,
         'Allt af þessu.',
         true
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         3,
         'Nota eingöngu lyklaborð við að skoða og nota vefinn.',
         false
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         3,
         'Merkja form á aðgengilegan hátt.',
         false
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         3,
         'Hafa tóman alt texta á myndum ef þær eru eingöngu til skrauts.',
         false
     );
-INSERT INTO spurningar (flokkur_id, spurning)
+INSERT INTO questions (category_id, text)
 VALUES (
         1,
         'Hvað er merkingarfræði í sambandi við námsefnið?'
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         4,
         'Hvert HTML element hefur einhverja skilgreinda merkingu—merkingarfræðilegt gildi—sem við þurfum að hafa í huga þegar við smíðum vefi.',
         true
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         4,
         'Hvert HTML tag hefur einhverja skilgreinda merkingu—merkingarfræðilegt gildi—sem við þurfum að hafa í huga þegar við smíðum vefi.',
         false
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         4,
         'Hvert CSS eigindi hefur einhverja skilgreinda merkingu—merkingarfræðilegt gildi—sem við þurfum að hafa í huga þegar við smíðum vefi.',
         false
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (
         4,
         'Hver CSS selector hefur einhverja skilgreinda merkingu—merkingarfræðilegt gildi—sem við þurfum að hafa í huga þegar við smíðum vefi.',
         false
     );
-INSERT INTO spurningar (flokkur_id, spurning)
+INSERT INTO questions (category_id, text)
 VALUES (
         2,
         'Fyrir eftirfarandi HTML / for the following HTML:\n\n\n<div class="text">\n  <h1 class="important text__title">Halló heimur</p>\n</div>\n \n\nEr skilgreint CSS / there is defined CSS:\n\n\n.text {\n  font-size: 10px;\n  color: green;\n}\n\n.text .text__title {\n  font-size: 1.5em;\n}\n\n.important {\n  font-size: 2em;\n  color: red;\n}\n\n \n\nHvert af eftirfarandi er satt fyrir textann „Halló heimur“ eftir að búið er að reikna gildi?'
@@ -138,7 +138,7 @@ VALUES (
         2,
         'Þegar við notum flexbox hvað af eftirfarandi er satt? Gerið ráð fyrir að skjal sé lesið frá vinstri til hægri.'
     );
-INSERT INTO svor (spurning_id, svar, rett_svar)
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (1, 'font-size: 20px;, color: green;', true),
     (1, 'font-size: 15px;, color: red;', false),
     (1, 'font-size: 20px;, color: red;', false),
@@ -199,8 +199,8 @@ VALUES (1, 'font-size: 20px;, color: green;', true),
         'Höfum skilgreinda tvo ása: aðalás og krossás sem eru samsíða; sjálfgefin röðun er á krossás frá vinstri til hægri.',
         false
     );
--- Insert into spurningar table
-INSERT INTO spurningar (flokkur_id, spurning)
+-- Insert into questions table
+INSERT INTO questions (category_id, text)
 VALUES (
         3,
         'Hvað er skrifað út eftir að eftirfarandi kóði er keyrður?\n\nconst items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];\n\nconst іtem = items\n  .map((i) => i * 2)\n  .filter(\n    (i) => i < 10\n  )\n  .find((i) => i > 6)\n\n\nconsole.log(item);'
@@ -213,8 +213,8 @@ VALUES (
         3,
         'Þegar við notum `fetch` í JavaScript ætti ferlið við að sækja gögn að vera'
     );
--- Insert into svor table
-INSERT INTO svor (spurning_id, svar, rett_svar)
+-- Insert into answers table
+INSERT INTO answers (question_id, text, is_correct)
 VALUES (1, '8', true),
     (1, '[8]', false),
     (
@@ -245,21 +245,21 @@ VALUES (1, '8', true),
     ),
     (
         3,
-        'Búið til `fetch` request kall sem tilgreinir að minnsta kosti URL; villuathugun á kalli og svari með tilliti til HTTP; gögn sótt í response með villuathugun.',
+        'Búið til `fetch` request kall sem tilgreinir að minnsta kosti URL; villuathugun á kalli og texti með tilliti til HTTP; gögn sótt í response með villuathugun.',
         true
     ),
     (
         3,
-        'Búið til `fetch` request kall sem verður að tilgreina URL, HTTP aðferð og stöðukóða; villuathugun á kalli og svari með tilliti til HTTP; gögn sótt í response með villuathugun.',
+        'Búið til `fetch` request kall sem verður að tilgreina URL, HTTP aðferð og stöðukóða; villuathugun á kalli og texti með tilliti til HTTP; gögn sótt í response með villuathugun.',
         false
     ),
     (
         3,
-        'Búið til `fetch` request kall sem tilgreinir að minnsta kosti URL; villuathugun á kalli og svari með tilliti til URL; gögn sótt í response.',
+        'Búið til `fetch` request kall sem tilgreinir að minnsta kosti URL; villuathugun á kalli og texti með tilliti til URL; gögn sótt í response.',
         false
     ),
     (
         3,
-        'Búið til `fetch` request kall sem tilgreinir að minnsta kosti URL; villuathugun á kalli og svari með tilliti til HTTP; eingöngu JSON gögn sótt í response með villuathugun.',
+        'Búið til `fetch` request kall sem tilgreinir að minnsta kosti URL; villuathugun á kalli og texti með tilliti til HTTP; eingöngu JSON gögn sótt í response með villuathugun.',
         false
     );
